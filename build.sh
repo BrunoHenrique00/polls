84e9b2a9b82b019f75dbb52d2fc0b69948103462
bruno henrique <bh.bsb2011@hotmail.com>
2023-08-08 11:09:29 -0300

    deploy it

---
+++ build.sh
@@ -0,0 +1,10 @@
+echo "BUILDING DJANGO APP"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "MIGRATING DB"
+
+python3.9 manage.py migrate
+
+echo "BUILD FINISHED"
+
